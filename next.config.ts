f6db669bc82cb3999d4b9acada3a36fd0f40b7f8
argalusmp <argalus03@gmail.com>
2025-05-15 11:22:01 +0700

    update enable http

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://143.198.214.90/:path*',
+      },
+    ]
+  },
 };
 
 export default nextConfig;
