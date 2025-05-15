547ad4b1a23726c29c3d7cda2239db7a1595f845
argalusmp <argalus03@gmail.com>
2025-05-15 12:45:53 +0700

    update nextconfig

--- next.config.ts
+++ next.config.ts
@@ -2,20 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  async rewrites() {
-    // Only apply rewrites in development
-    if (process.env.NODE_ENV === 'development') {
-      return [
-        {
-          source: '/api/:path*',
-          destination: 'http://143.198.214.90/:path*', // Your local API server
-        },
-      ];
-    }
-    
-    // In production, return an empty array (no rewrites)
-    return [];
-  },
 };
 
 export default nextConfig;
